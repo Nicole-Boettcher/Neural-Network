890718647471915c74b492681a4a3db007a73e12
Nicole-Boettcher <71027559+Nicole-Boettcher@users.noreply.github.com>
2023-11-22 16:41:07 -0500

    Training loss is less than validation loss

--- boettchn_400262726_A4_code.py
+++ boettchn_400262726_A4_code.py
@@ -1,13 +1,15 @@
 import numpy as np
 import math
+import random
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 
 random_seed = 7262 
-num_nodes_layer_1 = 3
-num_nodes_layer_2 = 3
-alpha = 0.05
+num_nodes_layer_1 = 2
+num_nodes_layer_2 = 4
+num_epoch = 20
 
 def pre_processing():
     feature_data = []
@@ -24,7 +26,7 @@ def pre_processing():
             index = index + 1
 
     # split the data
-    X_train, X_test_val, t_train, t_test_val = train_test_split(feature_data, target_data, test_size = 0.3, random_state =random_seed)  # split data in 2 sets
+    X_train, X_test_val, t_train, t_test_val = train_test_split(feature_data, target_data, test_size = 0.2, random_state =random_seed)  # split data in 2 sets
     X_test, X_val, t_test, t_val = train_test_split(X_test_val, t_test_val, test_size = 0.5, random_state =random_seed)  # split data in 2 sets
 
     return np.array(sc.fit_transform(X_train)), np.array(t_train), np.array(sc.transform(X_val)), np.array(t_val), np.array(sc.transform(X_test)), np.array(t_test)
@@ -75,9 +77,9 @@ def forward_propagation_test(x_train, weights_layer_1, weights_layer_2, weights_
     
     z_1, output_layer_1 = process_layer(x_train, weights_layer_1, True)
     z_2, output_layer_2 = process_layer(output_layer_1, weights_layer_2, True)
-    z_3, final_output = process_layer(output_layer_2, weights_output, False)
-    output_class = bayes_classifer(final_output)    #classify the probability
-    return output_class
+    z_3, h_3 = process_layer(output_layer_2, weights_output, False) #z_3 is before sigmoid, h_3 is after sigmoid - h_3 = sigmoid(z_3)
+    output_class = bayes_classifer(h_3)    #classify the probability
+    return z_3, h_3, output_class
 
 
 def forward_propagation(x_train, weights_layer_1, weights_layer_2, weights_output):
@@ -91,9 +93,9 @@ def forward_propagation(x_train, weights_layer_1, weights_layer_2, weights_outpu
     z_1, output_layer_1 = process_layer(x_train, weights_layer_1, True)
     z_2, output_layer_2 = process_layer(output_layer_1, weights_layer_2, True)
     z_3, final_output = process_layer(output_layer_2, weights_output, False)
-    output_class = bayes_classifer(final_output)    #classify the probability
-    print("output class = ", output_class)
-    return z_1, output_layer_1, z_2, output_layer_2, z_3, final_output
+    pred_class = bayes_classifer(final_output)    #classify the probability
+    #print("z output = ", final_output)
+    return z_1, output_layer_1, z_2, output_layer_2, z_3, final_output, pred_class
 
 def bayes_classifer(output):
     if output <= 0.5:
@@ -135,37 +137,150 @@ def derivative_relu(z):
     conditional = z < 0
     return np.where(conditional, 0, 1)  # if each element in z is less than 0, than the slope is 0 and if z > 0 then the slope is 1
 
+from scipy.special import expit
+
+def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
+    cross_entro_loss = 0
+    accuracy = 0
+
+    for index in range(len(x_set)):
+        pre_sigmoid, h_3, output_class = forward_propagation_test(x_set[index], weights_layer_1, weights_layer_2, weights_output)
+        sigmoid_value = expit(pre_sigmoid[0])
+
+        cross_entro_loss += float(t_set[index]) * np.log(sigmoid_value) + (1 - float(t_set[index])) * np.log(1 - sigmoid_value)
+
+        if output_class == int(t_set[index]): 
+            accuracy += 1
+
+    average_cross_entro_loss = -cross_entro_loss / len(x_set)  # Negative sign to convert to average and handle minimization
+    print("Average Cross-Entropy Loss:", average_cross_entro_loss)
+    print("Accuracy:", accuracy / len(x_set))
+
+    return average_cross_entro_loss
+
+
+def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
+    # need to run all validation samples throught the net and get average cross entro loss
+    cross_entro_loss = 0
+    accuracy = 0
+    for index in range(len(x_set)):
+        pre_sigmoid, h_3, output_class = forward_propagation_test(x_set[index], weights_layer_1, weights_layer_2, weights_output)
+        cross_entro_loss += float(t_set[index])*np.logaddexp(0,-float(pre_sigmoid[0])) + (1-float(t_set[index]))*np.logaddexp(0,float(pre_sigmoid[0]))
+
+        if output_class == int(t_set[index]): 
+            accuracy = accuracy + 1
+
+    print("Accuracy = ", accuracy / len(t_set), "       cross_entro_loss = ", cross_entro_loss / len(x_set))
+    return cross_entro_loss / len(x_set)
+
+
+# def cross_entro_loss(t_set_element, pre_sigmoid):
+#     return float(t_set_element)*np.logaddexp(0,-float(pre_sigmoid[0])) + (1-float(t_set_element))*np.logaddexp(0,float(pre_sigmoid[0]))
+
+# def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
+#     # need to run all validation samples throught the net and get average cross entro loss
+#     cross_entro_loss = 0
+#     accuracy = 0
+
+#     pre_sigmoid, z_3, output_class = forward_propagation_test(x_set, weights_layer_1, weights_layer_2, weights_output)  #act the forward propagation on the whole x_set
+#     cross_entro_loss = cross_entro_loss(t_set, pre_sigmoid)
+
+#     return np.average(cross_entro_loss)
 
 def main():
     print("Assignment 4")
     #input_data = np.array((1372,5), float)
 
-    x_train, t_train, x_val, t_val, x_test, t_test = pre_processing()
+    x_train, t_train, x_test, t_test, x_val, t_val = pre_processing()
+    print(x_train.shape)
+    print(t_train.shape)
+    print(x_test.shape)
+    print(t_test.shape)
+    print(x_val.shape)
+    print(t_val.shape)
     # need weights for each layer - 3 layers - [[3,5],[3,4],[1,4]]
-    weights_layer_1, weights_layer_2, weights_output = init_weights()
+    indices_train = np.arange(len(x_train))
+    indices_val = np.arange(len(x_val))
 
-    for sample_num in range(len(x_train)):
-        z_1, output_layer_1, z_2, output_layer_2, z_3, final_output = forward_propagation(x_train[sample_num], weights_layer_1, weights_layer_2, weights_output)
-        gradient_weights = back_propagation(x_train[sample_num], t_train[sample_num], weights_layer_1, z_1, output_layer_1, weights_layer_2, z_2, output_layer_2, weights_output, z_3)
+    graph = 0
+    alphas = [0.003]
+    for alpha in alphas:
+        weights_layer_1, weights_layer_2, weights_output = init_weights()
+        training_cross_entro_loss = []
+        validation_cross_entro_loss = []
+        test = []
 
-        # update weights using gradient 
-        weights_layer_1 = weights_layer_1 - alpha*(gradient_weights[0])
-        weights_layer_2 = weights_layer_2 - alpha*(gradient_weights[1])
-        weights_output = weights_output - alpha*(gradient_weights[2])
-        print("Weights updated: ", sample_num)
+        for epoch in range(num_epoch):
+            print("Epoch = ", epoch)
+            np.random.shuffle(indices_train)
+            x_train = x_train[indices_train]
+            t_train = t_train[indices_train]
+            np.random.shuffle(indices_val)
+            x_val = x_val[indices_val]
+            t_val = t_val[indices_val]
 
-    correct_pred = 0
-    for test_sample_num in range(len(x_test)):
-        pred_class = forward_propagation_test(x_test[test_sample_num], weights_layer_1, weights_layer_2, weights_output)
-        #print("\ntarget = ", t_test[test_sample_num], " predicited = ", pred_class)
-        if pred_class == int(t_test[test_sample_num]): 
-            #print("correct")
-            correct_pred = correct_pred + 1
+            for sample_num in range(len(x_train)):
+                z_1, output_layer_1, z_2, output_layer_2, z_3, final_output, pred_class = forward_propagation(x_train[sample_num], weights_layer_1, weights_layer_2, weights_output)
+                gradient_weights = back_propagation(x_train[sample_num], t_train[sample_num], weights_layer_1, z_1, output_layer_1, weights_layer_2, z_2, output_layer_2, weights_output, z_3)
 
-    print(weights_layer_1)
-    print(weights_layer_2)
-    print(weights_output)
-    print("accuracy = ", correct_pred / len(t_test))
+                # update weights using gradient 
+                weights_layer_1 = weights_layer_1 - alpha*(gradient_weights[0])
+                weights_layer_2 = weights_layer_2 - alpha*(gradient_weights[1])
+                weights_output = weights_output - alpha*(gradient_weights[2])
+
+                # calculate training and validation error
+                #print("Weights updated: ", sample_num)
+
+            print("Training: ")
+            training_loss = calculate_loss(x_train, t_train, weights_layer_1, weights_layer_2, weights_output)
+            print("Validation: ")
+            val_loss = calculate_loss(x_val, t_val, weights_layer_1, weights_layer_2, weights_output)
+            #test_loss = calculate_loss(x_test, t_test, weights_layer_1, weights_layer_2, weights_output)
+            #print("Val Loss = ", val_loss)
+            #print("Training loss = ", training_loss)
+            validation_cross_entro_loss.append(val_loss)
+            training_cross_entro_loss.append(training_loss)
+            #test.append(test_loss)
+
+
+        graph += 1
+        title = "Alpha = " , alpha
+        plt.subplot(1, 1, graph)
+        plt.title(title)
+        plt.xlabel("Epoch")
+        plt.ylabel("Cross entropy loss")
+        plt.plot(training_cross_entro_loss, 'r:x', label="training")
+        plt.plot(validation_cross_entro_loss, 'b:x', label="validation")
+        #plt.plot(test, 'g:x', label="test")
+        plt.legend()
+        
+    plt.suptitle("Training and Validation cross entropy loss per Epoch - (3,2)")
+    plt.show()
+
+    # correct_pred = 0
+    # for test_sample_num in range(len(x_test)):
+    #     z_3, pred_class = forward_propagation_test(x_test[test_sample_num], weights_layer_1, weights_layer_2, weights_output)
+    #     #print("\ntarget = ", t_test[test_sample_num], " predicited = ", pred_class)
+    #     if pred_class == int(t_test[test_sample_num]): 
+    #         #print("correct")
+    #         correct_pred = correct_pred + 1
+
+    # print(weights_layer_1)
+    # print(weights_layer_2)
+    # print(weights_output)
+    # print("accuracy = ", correct_pred / len(t_test))
+
+
+    # correct_pred_train = 0
+    # for train_sample_num in range(len(x_train)):
+    #     pre_sigmoid, z_3, pred_class = forward_propagation_test(x_train[train_sample_num], weights_layer_1, weights_layer_2, weights_output)
+    #     print("\ntarget = ", t_train[train_sample_num], " predicited = ", pred_class)
+    #     if pred_class == int(t_train[train_sample_num]): 
+    #         print("correct")
+    #         correct_pred_train = correct_pred_train + 1
+
+    # print("TRAINING accuracy = ", correct_pred_train / len(t_train))
+
 
 
 main()
