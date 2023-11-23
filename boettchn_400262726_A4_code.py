b74496731c71d4be049cd8e021c81e95889ee9d2
Nicole-Boettcher <71027559+Nicole-Boettcher@users.noreply.github.com>
2023-11-23 10:52:49 -0500

    Testing with different number of nodes

--- boettchn_400262726_A4_code.py
+++ boettchn_400262726_A4_code.py
@@ -4,12 +4,13 @@ import random
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from scipy.special import expit
 sc = StandardScaler()
 
 random_seed = 7262 
 num_nodes_layer_1 = 2
-num_nodes_layer_2 = 4
-num_epoch = 20
+num_nodes_layer_2 = 2
+num_epoch = 250
 
 def pre_processing():
     feature_data = []
@@ -31,24 +32,92 @@ def pre_processing():
 
     return np.array(sc.fit_transform(X_train)), np.array(t_train), np.array(sc.transform(X_val)), np.array(t_val), np.array(sc.transform(X_test)), np.array(t_test)
 
-def init_weights():
+def init_weights(i):
 
-    lower_bound_l1 = -1/math.sqrt(4)
-    lower_bound_l2 = -1/math.sqrt(num_nodes_layer_1)
-    lower_bound_l3 = -1/math.sqrt(num_nodes_layer_2)
-    
-    weights_layer_1 = np.random.uniform(lower_bound_l1, -1*lower_bound_l1, size=(num_nodes_layer_1, 5))
-    weights_layer_2 = np.random.uniform(lower_bound_l2, -1*lower_bound_l2, size=(num_nodes_layer_2, num_nodes_layer_1+1))
-    weights_output = np.random.uniform(lower_bound_l3, -1*lower_bound_l3, size=(1, num_nodes_layer_2+1))
+    if i == 'Uniform 1':
+        lower_bound_l1 = -1/math.sqrt(4)
+        lower_bound_l2 = -1/math.sqrt(num_nodes_layer_1)
+        lower_bound_l3 = -1/math.sqrt(num_nodes_layer_2)
+        
+        weights_layer_1 = np.random.uniform(lower_bound_l1, -1*lower_bound_l1, size=(num_nodes_layer_1, 5))
+        weights_layer_2 = np.random.uniform(lower_bound_l2, -1*lower_bound_l2, size=(num_nodes_layer_2, num_nodes_layer_1+1))
+        weights_output = np.random.uniform(lower_bound_l3, -1*lower_bound_l3, size=(1, num_nodes_layer_2+1))
+        weights_layer_1[:, 0] = 0
+        weights_layer_2[:, 0] = 0
+        weights_output[:, 0] = 0
 
-    weights_layer_1[:, 0] = 0
-    weights_layer_2[:, 0] = 0
-    weights_output[:, 0] = 0
-    
+    elif i == 'Uniform 2':
+        lower_bound_l1 = -math.sqrt(6/(4+num_nodes_layer_1))
+        lower_bound_l2 = -math.sqrt(6/(num_nodes_layer_1+num_nodes_layer_2))
+        lower_bound_l3 = -math.sqrt(6/(num_nodes_layer_2+1))
+        
+        weights_layer_1 = np.random.uniform(lower_bound_l1, -1*lower_bound_l1, size=(num_nodes_layer_1, 5))
+        weights_layer_2 = np.random.uniform(lower_bound_l2, -1*lower_bound_l2, size=(num_nodes_layer_2, num_nodes_layer_1+1))
+        weights_output = np.random.uniform(lower_bound_l3, -1*lower_bound_l3, size=(1, num_nodes_layer_2+1))
+        weights_layer_1[:, 0] = 0
+        weights_layer_2[:, 0] = 0
+        weights_output[:, 0] = 0
+
+    elif i == 'Kaiming He':
+        weights_layer_1 = np.random.randn(num_nodes_layer_1, 5) * np.sqrt(2.0 / 4)
+        weights_layer_2 = np.random.randn(num_nodes_layer_2, num_nodes_layer_1+1) * np.sqrt(2.0 / num_nodes_layer_1+1)
+        weights_output = np.random.randn(1, num_nodes_layer_2+1) * np.sqrt(2.0 / num_nodes_layer_2 + 1)
+        weights_layer_1[:, 0] = 0
+        weights_layer_2[:, 0] = 0
+        weights_output[:, 0] = 0
+        
     return weights_layer_1, weights_layer_2, weights_output
 
 def relu(x):
     return np.maximum(0, x)
+
+def process_layer_test_1(inputs, weights, hidden_true):
+    #print("Processing hidden layer")
+    # output = h
+    # z = weights * nodes
+    # h = activate(z)
+
+    # add dummy feature to inputs 
+    inputs_w_dummy = np.insert(inputs, 0, 1, axis=1)
+
+    z = np.dot(weights, inputs_w_dummy.T)     # should have size of (number of nodes, 1)
+    if hidden_true:
+        h = relu(z)     # should have size of (number of nodes, 1) -- check this works with array input to relu
+    else:
+        h = 1 / (1 + np.exp(-z))
+
+    return z, h
+
+def process_layer_test_2(inputs, weights, hidden_true):
+    #print("Processing hidden layer")
+    # output = h
+    # z = weights * nodes
+    # h = activate(z)
+
+    # add dummy feature to inputs 
+    inputs_w_dummy = np.insert(inputs, 0, 1, axis=0)
+
+    z = np.dot(weights, inputs_w_dummy)     # should have size of (number of nodes, 1)
+    if hidden_true:
+        h = relu(z)     # should have size of (number of nodes, 1) -- check this works with array input to relu
+    else:
+        h = 1 / (1 + np.exp(-z))
+
+    return z, h
+
+def forward_propagation_test(x_train, weights_layer_1, weights_layer_2, weights_output):
+    #print("Starting forward propagation")
+
+    #init weights to all ones and segment by layers 
+
+    # call process layer for each layer 
+    # structure is 4 inputs, 1st hidden layer is 3 nodes, 2nd hidden layer is 3 nodes, output is 1 value with sigmoid
+    
+    z_1, output_layer_1 = process_layer_test_1(x_train, weights_layer_1, True)
+    z_2, output_layer_2 = process_layer_test_2(output_layer_1, weights_layer_2, True)
+    z_3, h_3 = process_layer_test_2(output_layer_2, weights_output, False) #z_3 is before sigmoid, h_3 is after sigmoid - h_3 = sigmoid(z_3)
+    output_class = bayes_classifer(h_3)    #classify the probability
+    return z_3, h_3, output_class
 
 def process_layer(inputs, weights, hidden_true):
     #print("Processing hidden layer")
@@ -67,21 +136,6 @@ def process_layer(inputs, weights, hidden_true):
 
     return z, h
 
-def forward_propagation_test(x_train, weights_layer_1, weights_layer_2, weights_output):
-    #print("Starting forward propagation")
-
-    #init weights to all ones and segment by layers 
-
-    # call process layer for each layer 
-    # structure is 4 inputs, 1st hidden layer is 3 nodes, 2nd hidden layer is 3 nodes, output is 1 value with sigmoid
-    
-    z_1, output_layer_1 = process_layer(x_train, weights_layer_1, True)
-    z_2, output_layer_2 = process_layer(output_layer_1, weights_layer_2, True)
-    z_3, h_3 = process_layer(output_layer_2, weights_output, False) #z_3 is before sigmoid, h_3 is after sigmoid - h_3 = sigmoid(z_3)
-    output_class = bayes_classifer(h_3)    #classify the probability
-    return z_3, h_3, output_class
-
-
 def forward_propagation(x_train, weights_layer_1, weights_layer_2, weights_output):
     #print("Starting forward propagation")
 
@@ -98,10 +152,7 @@ def forward_propagation(x_train, weights_layer_1, weights_layer_2, weights_outpu
     return z_1, output_layer_1, z_2, output_layer_2, z_3, final_output, pred_class
 
 def bayes_classifer(output):
-    if output <= 0.5:
-        return 0
-    else:
-        return 1
+    return (output > 0.5).astype(int)
     
 def back_propagation(x_train, t_train, weights_layer_1, z_1, output_layer_1, weights_layer_2, z_2, output_layer_2, weights_output, z_3):
     #print("Back Propagation")
@@ -137,55 +188,33 @@ def derivative_relu(z):
     conditional = z < 0
     return np.where(conditional, 0, 1)  # if each element in z is less than 0, than the slope is 0 and if z > 0 then the slope is 1
 
-from scipy.special import expit
-
-def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
-    cross_entro_loss = 0
-    accuracy = 0
-
-    for index in range(len(x_set)):
-        pre_sigmoid, h_3, output_class = forward_propagation_test(x_set[index], weights_layer_1, weights_layer_2, weights_output)
-        sigmoid_value = expit(pre_sigmoid[0])
-
-        cross_entro_loss += float(t_set[index]) * np.log(sigmoid_value) + (1 - float(t_set[index])) * np.log(1 - sigmoid_value)
-
-        if output_class == int(t_set[index]): 
-            accuracy += 1
-
-    average_cross_entro_loss = -cross_entro_loss / len(x_set)  # Negative sign to convert to average and handle minimization
-    print("Average Cross-Entropy Loss:", average_cross_entro_loss)
-    print("Accuracy:", accuracy / len(x_set))
-
-    return average_cross_entro_loss
-
-
-def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
-    # need to run all validation samples throught the net and get average cross entro loss
-    cross_entro_loss = 0
-    accuracy = 0
-    for index in range(len(x_set)):
-        pre_sigmoid, h_3, output_class = forward_propagation_test(x_set[index], weights_layer_1, weights_layer_2, weights_output)
-        cross_entro_loss += float(t_set[index])*np.logaddexp(0,-float(pre_sigmoid[0])) + (1-float(t_set[index]))*np.logaddexp(0,float(pre_sigmoid[0]))
-
-        if output_class == int(t_set[index]): 
-            accuracy = accuracy + 1
-
-    print("Accuracy = ", accuracy / len(t_set), "       cross_entro_loss = ", cross_entro_loss / len(x_set))
-    return cross_entro_loss / len(x_set)
-
-
-# def cross_entro_loss(t_set_element, pre_sigmoid):
-#     return float(t_set_element)*np.logaddexp(0,-float(pre_sigmoid[0])) + (1-float(t_set_element))*np.logaddexp(0,float(pre_sigmoid[0]))
-
 # def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
 #     # need to run all validation samples throught the net and get average cross entro loss
 #     cross_entro_loss = 0
 #     accuracy = 0
+#     for index in range(len(x_set)):
+#         pre_sigmoid, h_3, output_class = forward_propagation_test(x_set[index], weights_layer_1, weights_layer_2, weights_output)
+#         cross_entro_loss += float(t_set[index])*np.logaddexp(0,-float(pre_sigmoid[0])) + (1-float(t_set[index]))*np.logaddexp(0,float(pre_sigmoid[0]))
 
-#     pre_sigmoid, z_3, output_class = forward_propagation_test(x_set, weights_layer_1, weights_layer_2, weights_output)  #act the forward propagation on the whole x_set
-#     cross_entro_loss = cross_entro_loss(t_set, pre_sigmoid)
+#         if output_class == int(t_set[index]): 
+#             accuracy = accuracy + 1
 
-#     return np.average(cross_entro_loss)
+#     print("Accuracy = ", accuracy / len(t_set), "       cross_entro_loss = ", cross_entro_loss / len(x_set))
+#     return cross_entro_loss / len(x_set)
+
+
+# VECTORIZED loss function
+def calculate_loss(x_set, t_set, weights_layer_1, weights_layer_2, weights_output):
+    # need to run all validation samples through the net and get average cross entropy loss
+    pre_sigmoid, _, _ = forward_propagation_test(x_set, weights_layer_1, weights_layer_2, weights_output)    
+    # Compute the cross-entropy loss for all samples
+    t_set = np.array(t_set).astype('float64')
+    pre_sigmoid = np.array(pre_sigmoid).astype('float64')
+
+    cross_entro_loss = np.average(t_set * np.logaddexp(0, -pre_sigmoid) + (1 - t_set) * np.logaddexp(0, pre_sigmoid))
+    #print(cross_entro_loss)
+    return cross_entro_loss # Negative sign to convert to average and handle minimization
+
 
 def main():
     print("Assignment 4")
@@ -201,17 +230,18 @@ def main():
     # need weights for each layer - 3 layers - [[3,5],[3,4],[1,4]]
     indices_train = np.arange(len(x_train))
     indices_val = np.arange(len(x_val))
+    final_val_train_loss = []
 
     graph = 0
-    alphas = [0.003]
-    for alpha in alphas:
-        weights_layer_1, weights_layer_2, weights_output = init_weights()
+    alpha = 0.005
+    weight_init = ['Uniform 1', 'Uniform 2', 'Kaiming He']
+    for weights in weight_init:
+        weights_layer_1, weights_layer_2, weights_output = init_weights(weights)
         training_cross_entro_loss = []
         validation_cross_entro_loss = []
-        test = []
 
         for epoch in range(num_epoch):
-            print("Epoch = ", epoch)
+            #print("Epoch = ", epoch)
             np.random.shuffle(indices_train)
             x_train = x_train[indices_train]
             t_train = t_train[indices_train]
@@ -231,9 +261,9 @@ def main():
                 # calculate training and validation error
                 #print("Weights updated: ", sample_num)
 
-            print("Training: ")
+            #print("Training: ")
             training_loss = calculate_loss(x_train, t_train, weights_layer_1, weights_layer_2, weights_output)
-            print("Validation: ")
+            #print("Validation: ")
             val_loss = calculate_loss(x_val, t_val, weights_layer_1, weights_layer_2, weights_output)
             #test_loss = calculate_loss(x_test, t_test, weights_layer_1, weights_layer_2, weights_output)
             #print("Val Loss = ", val_loss)
@@ -244,17 +274,28 @@ def main():
 
 
         graph += 1
-        title = "Alpha = " , alpha
-        plt.subplot(1, 1, graph)
+        title = "Weight init: " , weights
+        plt.subplot(3, 1, graph)
         plt.title(title)
         plt.xlabel("Epoch")
         plt.ylabel("Cross entropy loss")
+        plt.ylim(0,0.2)
         plt.plot(training_cross_entro_loss, 'r:x', label="training")
         plt.plot(validation_cross_entro_loss, 'b:x', label="validation")
         #plt.plot(test, 'g:x', label="test")
         plt.legend()
+        print(weights)
+        print("Training loss = ", training_cross_entro_loss[-1])
+        print("Validation loss = ", validation_cross_entro_loss[-1])
+        print("Layer 1 weights: ", weights_layer_1)
+        print("Layer 2 weights: ", weights_layer_2)
+        print("Layer 3 weights: ", weights_output)
+        final_val_train_loss.append(training_cross_entro_loss[-1])
+        final_val_train_loss.append(validation_cross_entro_loss[-1])
         
-    plt.suptitle("Training and Validation cross entropy loss per Epoch - (3,2)")
+    final = [round(element, 5) for element in final_val_train_loss]
+    print(final)
+    plt.suptitle("Training and Validation cross entropy loss per Epoch - (2,2)")
     plt.show()
 
     # correct_pred = 0
